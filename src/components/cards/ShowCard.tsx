eb34503507f4299e1c3053392e2e91da86a0ad85
gflorejsfun <gfloresjswork@gmail.com>
2022-11-21 03:56:57 +0100

    move buttons of show card to the right

--- src/components/cards/ShowCard.tsx
+++ src/components/cards/ShowCard.tsx
@@ -11,14 +11,14 @@ const ShowCard: React.FC<ShowCardProps> = ({ item: { id, title, duration, coverS
   <MainCard content={false} boxShadow>
     <Stack direction="row">
       <CardMedia component="img" sx={{ height: 150, width: 150 }} image={coverS3Url} />
-      <Stack direction="column">
+      <Stack direction="column" flexGrow={1}>
         <CardContent>
           <Typography align="left" variant="h5" sx={{ mb: 1 }}>
             {title}
           </Typography>
           <Chip icon={<VideoFileIcon />} label={formatDuration(duration * 1000)} variant="outlined" size="small" />
         </CardContent>
-        <CardActions>
+        <CardActions sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
           <IconButton aria-label="add to favorites" color="primary" size="small" component={Link} to={`${id}/edit`}>
             <EditIcon />
           </IconButton>
